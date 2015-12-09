bf86c5bb381a3edcbc809f9d62782c99b50de544
simpkae <simpkae@gmail.com>
2015-12-09 13:28:21 +0800

    add sql

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLALchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-from app import views
+db = SQLALchemy(app)
+
+from app import views, models
